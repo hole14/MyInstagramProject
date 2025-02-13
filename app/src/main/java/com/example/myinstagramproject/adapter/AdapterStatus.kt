a258d7aa05e990a9174418d87a067a11908efc7d
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 08:19:24 +0700

    Mengubah AdapterStatus.kt

--- app/src/main/java/com/example/myinstagramproject/adapter/AdapterStatus.kt
+++ app/src/main/java/com/example/myinstagramproject/adapter/AdapterStatus.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myinstagramproject.R
 import com.example.myinstagramproject.deskripsi.DeskripsiStatus
 import de.hdodenhof.circleimageview.CircleImageView
 
-class AdapterStatus(private val list: ArrayList<tampilan>): RecyclerView.Adapter<AdapterStatus.listViewHolder>() {
+class AdapterStatus(private val list: MutableList<tampilan>): RecyclerView.Adapter<AdapterStatus.listViewHolder>() {
 
     interface itemDiklik{
         fun clikItem(data: tampilan)
@@ -26,7 +25,7 @@ class AdapterStatus(private val list: ArrayList<tampilan>): RecyclerView.Adapter
     companion object{
         const val EXTRA_NAME = "extra_name"
         const val EXTRA_PHOTO = "extra_photo"
-        const val EXTRA_STATUS = "extra_status"
+        const val EXTRA_POTO = "extra_poto"
     }
 
     class listViewHolder(lihat: View): RecyclerView.ViewHolder(lihat) {
@@ -40,7 +39,7 @@ class AdapterStatus(private val list: ArrayList<tampilan>): RecyclerView.Adapter
     }
 
     override fun onBindViewHolder(holder: listViewHolder, position: Int) {
-        val (judul, image) = list[position]
+        val (judul, image, poto) = list[position]
         holder.judul.text = judul
         holder.pp.setImageResource(image)
 
@@ -49,13 +48,21 @@ class AdapterStatus(private val list: ArrayList<tampilan>): RecyclerView.Adapter
             val intent = Intent(holder.itemView.context, DeskripsiStatus::class.java)
             intent.putExtra(EXTRA_NAME, judul)
             intent.putExtra(EXTRA_PHOTO, image)
+            intent.putExtra(EXTRA_POTO, poto)
             holder.itemView.context.startActivity(intent)
         }
     }
 
     override fun getItemCount(): Int = list.size
+
+    fun updateData(newList: MutableList<tampilan>) {
+        list.clear()
+        list.addAll(newList)
+        notifyDataSetChanged()
+    }
 }
 data class tampilan(
     val judul: String,
-    val image: Int
+    val image: Int,
+    val poto: Int
 )
