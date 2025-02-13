9c51a7c5e9f968a77ddb9b6a4247e1f4ab2b9eec
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 07:43:40 +0700

    Menambahkan viewModel

--- app/src/main/java/com/example/myinstagramproject/view_model/HomeViewModel.kt
+++ app/src/main/java/com/example/myinstagramproject/view_model/HomeViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.myinstagramproject.view_model
 
-class HomeViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.myinstagramproject.adapter.tampilan
+
+class HomeViewModel: ViewModel() {
+    val listData = MutableLiveData<MutableList<tampilan>>()
+
+    init {
+        listData.value = mutableListOf()
+    }
 }
