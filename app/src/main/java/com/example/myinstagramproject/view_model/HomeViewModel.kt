274c352061c0ae6c58b169beab59a2bd052c6e80
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-16 22:09:12 +0700

    Menambahkan HomeViewModel.kt

--- app/src/main/java/com/example/myinstagramproject/view_model/HomeViewModel.kt
+++ app/src/main/java/com/example/myinstagramproject/view_model/HomeViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.myinstagramproject.view_model
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.myinstagramproject.adapter.AdapterPostingan
+import com.example.myinstagramproject.adapter.postingan
 import com.example.myinstagramproject.adapter.tampilan
 
 class HomeViewModel: ViewModel() {
+
     val listData = MutableLiveData<MutableList<tampilan>>()
+    val listVideo = MutableLiveData<MutableList<postingan>>()
 
     init {
         listData.value = mutableListOf()
+        listVideo.value = mutableListOf()
     }
 }
