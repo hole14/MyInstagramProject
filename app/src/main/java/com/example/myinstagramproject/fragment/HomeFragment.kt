913818a773141022fa09c83c8708234ab566fb05
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-16 22:08:41 +0700

    Mengubah HomeFragment.kt

--- app/src/main/java/com/example/myinstagramproject/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/myinstagramproject/fragment/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myinstagramproject.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,15 +10,21 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myinstagramproject.R
+import com.example.myinstagramproject.adapter.AdapterPostingan
 import com.example.myinstagramproject.adapter.AdapterStatus
+import com.example.myinstagramproject.adapter.postingan
 import com.example.myinstagramproject.adapter.tampilan
 import com.example.myinstagramproject.databinding.FragmentHomeBinding
 import com.example.myinstagramproject.view_model.HomeViewModel
 
 class HomeFragment : Fragment() {
+
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
     private lateinit var status: RecyclerView
+    private lateinit var posting: RecyclerView
+
     private val viewModel: HomeViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,34 +33,69 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+
     }
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         status = binding.recyclerPp
         status.setHasFixedSize(true)
-        status.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        status.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
         val adapter = AdapterStatus(mutableListOf())
         status.adapter = adapter
 
-        if (viewModel.listData.value.isNullOrEmpty()){
-            viewModel.listData.value = tampilin()
-        }
-        viewModel.listData.observe(viewLifecycleOwner){ list ->
+        viewModel.listData.value = tampilin()
+
+        viewModel.listData.observe(viewLifecycleOwner) { list ->
             adapter.updateData(list)
         }
+
+        posting = binding.recyclerPosting
+        val adapterPosting = AdapterPostingan(mutableListOf())
+        posting.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            posting.adapter = adapterPosting
+        }
+        viewModel.listVideo.value = tampilinVideo()
+        viewModel.listVideo.observe(viewLifecycleOwner) { list ->
+            adapterPosting.list.apply {
+                clear()
+                addAll(list)
+            }
+            adapterPosting.notifyDataSetChanged()
+        }
+    }
+
+    private fun tampilinVideo(): MutableList<postingan> {
+        val dataJudul = resources.getStringArray(R.array.data_user)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_profile)
+        val dataVideo = resources.obtainTypedArray(R.array.data_postingan)
+
+        val listTampil = mutableListOf<postingan>()
+        for (i in dataJudul.indices) {
+            val tampil = postingan(dataJudul[i], dataPhoto.getResourceId(i, -1), dataVideo.getResourceId(i, -1))
+            listTampil.add(tampil)
+        }
+        dataVideo.recycle()
+        dataPhoto.recycle()
+        return listTampil
     }
 
     private fun tampilin(): MutableList<tampilan> {
         val dataJudul = resources.getStringArray(R.array.data_user)
         val dataPhoto = resources.obtainTypedArray(R.array.data_profile)
         val dataPoto = resources.obtainTypedArray(R.array.data_status)
-        val listTampil = mutableListOf<tampilan>()
-        for (i in dataJudul.indices) {
-            val tampil = tampilan(dataJudul[i], dataPhoto.getResourceId(i, -1), dataPoto.getResourceId(i, -1))
-            listTampil.add(tampil)
+
+        return List(dataJudul.size){ i ->
+            tampilan(dataJudul[i], dataPhoto.getResourceId(i, -1), dataPoto.getResourceId(i, -1))
+        }.toMutableList().also {
+            dataPhoto.recycle()
+            dataPoto.recycle()
         }
-        return listTampil
     }
 
     override fun onDestroy() {
