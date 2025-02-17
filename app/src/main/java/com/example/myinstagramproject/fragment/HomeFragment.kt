2ae44e1455fa855a02adba0645c42f002f2159fb
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-17 09:11:03 +0700

    Mengubah HomeFragment.kt

--- app/src/main/java/com/example/myinstagramproject/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/myinstagramproject/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,6 +19,8 @@ import com.example.myinstagramproject.databinding.FragmentHomeBinding
 import com.example.myinstagramproject.view_model.HomeViewModel
 
 class HomeFragment : Fragment() {
+
+    private lateinit var border: ImageView
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
